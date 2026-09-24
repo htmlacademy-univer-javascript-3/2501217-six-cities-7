5bc4e8c9993890e2dde8a46862306e188dc98103
imimorgo5 <ilyamuravyov@mail.ru>
2026-09-24 19:11:39 +0500

    Исправление форматирования и удаление лишних страниц

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -27,12 +27,20 @@ export const MainPage = ({ offerCount }: MainPageProps) => (
               <li className="header__nav-item user">
                 <a className="header__nav-link header__nav-link--profile" href="#">
                   <div className="header__avatar-wrapper user__avatar-wrapper" />
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <span className="header__favorite-count">3</span>
+                  <span className="header__user-name user__name">
+                    Oliver.conner@gmail.com
+                  </span>
+                  <span className="header__favorite-count">
+                    3
+                  </span>
                 </a>
               </li>
               <li className="header__nav-item">
-                <a className="header__nav-link" href="#"><span className="header__signout">Sign out</span></a>
+                <a className="header__nav-link" href="#">
+                  <span className="header__signout">
+                    Sign out
+                  </span>
+                </a>
               </li>
             </ul>
           </nav>
@@ -40,13 +48,19 @@ export const MainPage = ({ offerCount }: MainPageProps) => (
       </div>
     </header>
     <main className="page__main page__main--index">
-      <h1 className="visually-hidden">Cities</h1>
+      <h1 className="visually-hidden">
+        Cities
+      </h1>
       <div className="tabs">
         <section className="locations container">
           <ul className="locations__list tabs__list">
             {['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'].map((city) => (
               <li className="locations__item" key={city}>
-                <a className={`locations__item-link tabs__item${city === 'Amsterdam' ? ' tabs__item--active' : ''}`} href="#"><span>{city}</span></a>
+                <a className={`locations__item-link tabs__item${city === 'Amsterdam' ? ' tabs__item--active' : ''}`} href="#">
+                  <span>
+                    {city}
+                  </span>
+                </a>
               </li>
             ))}
           </ul>
@@ -55,12 +69,44 @@ export const MainPage = ({ offerCount }: MainPageProps) => (
       <div className="cities">
         <div className="cities__places-container container">
           <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{offerCount} places to stay in Amsterdam</b>
-            <form className="places__sorting" action="#" method="get"><span className="places__sorting-caption">Sort by</span><span className="places__sorting-type" tabIndex={0}>Popular<svg className="places__sorting-arrow" width="7" height="4"><use xlinkHref="#icon-arrow-select" /></svg></span><ul className="places__options places__options--custom places__options--opened"><li className="places__option places__option--active" tabIndex={0}>Popular</li><li className="places__option" tabIndex={0}>Price: low to high</li><li className="places__option" tabIndex={0}>Price: high to low</li><li className="places__option" tabIndex={0}>Top rated first</li></ul></form>
-            <div className="cities__places-list places__list tabs__content">{offers.map((offer) => <PlaceCard key={offer.id} {...offer} />)}</div>
+            <h2 className="visually-hidden">
+              Places
+            </h2>
+            <b className="places__found">
+              {offerCount} places to stay in Amsterdam
+            </b>
+            <form className="places__sorting" action="#" method="get">
+              <span className="places__sorting-caption">
+                Sort by
+              </span>
+              <span className="places__sorting-type" tabIndex={0}>
+                Popular
+                <svg className="places__sorting-arrow" width="7" height="4">
+                  <use xlinkHref="#icon-arrow-select" />
+                </svg>
+              </span>
+              <ul className="places__options places__options--custom places__options--opened">
+                <li className="places__option places__option--active" tabIndex={0}>
+                  Popular
+                </li>
+                <li className="places__option" tabIndex={0}>
+                  Price: low to high
+                </li>
+                <li className="places__option" tabIndex={0}>
+                  Price: high to low
+                </li>
+                <li className="places__option" tabIndex={0}>
+                  Top rated first
+                </li>
+              </ul>
+            </form>
+            <div className="cities__places-list places__list tabs__content">
+              {offers.map((offer) => <PlaceCard key={offer.id} {...offer} />)}
+            </div>
           </section>
-          <div className="cities__right-section"><section className="cities__map map" /></div>
+          <div className="cities__right-section">
+            <section className="cities__map map" />
+          </div>
         </div>
       </div>
     </main>
--- src/components/place-card/place-card.tsx
+++ src/components/place-card/place-card.tsx
@@ -25,7 +25,9 @@ export const PlaceCard = ({
     <article className="cities__card place-card">
       {isPremium && (
         <div className="place-card__mark">
-          <span>Premium</span>
+          <span>
+            Premium
+          </span>
         </div>
       )}
       <div className="cities__image-wrapper place-card__image-wrapper">
@@ -36,24 +38,38 @@ export const PlaceCard = ({
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{price}</b>
-            <span className="place-card__price-text">/&nbsp;night</span>
+            <b className="place-card__price-value">
+              &euro;{price}
+            </b>
+            <span className="place-card__price-text">
+              /&nbsp;night
+            </span>
           </div>
           <button className={bookmarkClassName} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark" />
             </svg>
-            <span className="visually-hidden">{isFavorite ? 'In bookmarks' : 'To bookmarks'}</span>
+            <span className="visually-hidden">
+              {isFavorite ? 'In bookmarks' : 'To bookmarks'}
+            </span>
           </button>
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
             <span style={{ width: ratingWidth }} />
-            <span className="visually-hidden">Rating</span>
+            <span className="visually-hidden">
+              Rating
+            </span>
           </div>
         </div>
-        <h2 className="place-card__name"><a href="#">{title}</a></h2>
-        <p className="place-card__type">{type}</p>
+        <h2 className="place-card__name">
+          <a href="#">
+            {title}
+          </a>
+        </h2>
+        <p className="place-card__type">
+          {type}
+        </p>
       </div>
     </article>
   );
